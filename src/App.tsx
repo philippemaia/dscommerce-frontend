ef66b5f2acea522103f38d1c70e3613658427511
Philippe Maia <philippe.m.maia@hotmail.com>
2026-04-01 01:53:05 -0300

    Página de detalhes em componentes React

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,23 @@
-import computerImg from './assets/computer.png';
-import cartImg from './assets/cart.svg';
 import './App.css'
+import computerImg from './assets/computer.png';
+import HeaderClient from './components/HeaderClient/index';
+import ButtonPrimary from './components/ButtonPrimary/index';
+import ButtonInverse from './components/ButtonInverse/index';
+import ProductDetailsCard from './components/ProductDetailsCard/index';
 
-function App() {
+export default function App() {
   return (
     <>
-        <header class="dsc-header-client">
-        <nav class="dsc-container">
-          <h1>DSCommerce</h1>
-          <div class="dsc-navbar-right">
-            <div class="dsc-menu-items-container">
-              <div class="dsc-menu-item">
-                <img src={cartImg} alt="Carrinho de compras" />
-              </div>
-            </div>
-            <a href="#">Entrar</a>
-          </div>
-        </nav>
-      </header>
+      <HeaderClient />
       <main>
         <section id="product-details-section" className="dsc-container">
-          <div class="dsc-card dsc-mb20">
-            <div class="dsc-product-details-top dsc-line-bottom">
-              <img src={computerImg} alt="Computador" />
-            </div>
-            <div class="dsc-product-details-bottom">
-              <h3>R$ 5000,00</h3>
-              <h4>Computador Gamer XT</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-              <div class="dsc-category-container">
-                  <div class="dsc-category">
-                      Eletrônicos
-                  </div>
-                  <div class="dsc-category">
-                      Computadores
-                  </div>
-              </div>
-            </div>
-          </div>
+          <ProductDetailsCard />
           <div class="dsc-btn-page-container">
-              <div class="dsc-btn dsc-btn-blue">
-                  Comprar
-              </div>
-              <div class="dsc-btn dsc-btn-white">
-                  Início
-              </div>
+              <ButtonPrimary />
+              <ButtonInverse />
           </div>
         </section>
       </main>
     </>
   );
 }
-
-export default App
---
+++ src/components/ProductCategory/index.tsx
@@ -0,0 +1,8 @@
+import './styles.css';
+
+export default function ProductCategory(){
+
+    return (
+        <div class="dsc-category">Eletrônicos</div>
+    );
+}
