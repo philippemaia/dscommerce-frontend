a03f961c2068c83b1da9727316a43dcbb2ec38d4
Philippe Maia <philippe.m.maia@hotmail.com>
2026-04-01 00:53:43 -0300

    Pagina de Detalhes com react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,62 @@
+import computerImg from './assets/computer.png';
+import cartImg from './assets/cart.svg';
 import './App.css'
 
 function App() {
   return (
-    <h1>DSCommerce 1234</h1>
+    <>
+        <header class="dsc-header-client">
+        <nav class="dsc-container">
+          <h1>DSCommerce</h1>
+          <div class="dsc-navbar-right">
+            <div class="dsc-menu-items-container">
+              <div class="dsc-menu-item">
+                <img src={cartImg} alt="Carrinho de compras" />
+              </div>
+            </div>
+            <a href="#">Entrar</a>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section id="product-details-section" className="dsc-container">
+          <div class="dsc-card dsc-mb20">
+            <div class="dsc-product-details-top dsc-line-bottom">
+              <img src={computerImg} alt="Computador" />
+            </div>
+            <div class="dsc-product-details-bottom">
+              <h3>R$ 5000,00</h3>
+              <h4>Computador Gamer XT</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <div class="dsc-category-container">
+                  <div class="dsc-category">
+                      Eletrônicos
+                  </div>
+                  <div class="dsc-category">
+                      Computadores
+                  </div>
+              </div>
+            </div>
+          </div>
+          <div class="dsc-btn-page-container">
+              <div class="dsc-btn dsc-btn-blue">
+                  Comprar
+              </div>
+              <div class="dsc-btn dsc-btn-white">
+                  Início
+              </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
