ec486453780f3f8e17313277fbf6c2934cc7d2c7
Philippe Maia <philippe.m.maia@hotmail.com>
2026-05-05 00:25:44 -0300

    Componente Props nos botões

--- src/components/ButtonInverse/index.tsx
+++ src/components/ButtonInverse/index.tsx
@@ -1,9 +1,13 @@
 import './styles.css';
 
-export default function ButtonInverse(){
+type Props = {
+    text: string;
+}
+
+export default function ButtonInverse({text}: Props){
     return (
         <div class="dsc-btn dsc-btn-white">
-            Início
+            {text}
         </div>
     );
 }
