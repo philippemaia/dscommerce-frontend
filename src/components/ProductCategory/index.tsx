a07551fcb671fdbf254103ceca9014e3c2abcf2e
Philippe Maia <philippe.m.maia@hotmail.com>
2026-05-05 00:18:53 -0300

    Componente Props na página Product Details

--- src/components/ProductCategory/index.tsx
+++ src/components/ProductCategory/index.tsx
@@ -1,8 +1,14 @@
 import './styles.css';
 
-export default function ProductCategory(){
+type Props = {
+    name: string;
+}
+
+export default function ProductCategory({name} : Props){
 
     return (
-        <div class="dsc-category">Eletrônicos</div>
+        <div class="dsc-category">
+            {name}
+        </div>
     );
 }
