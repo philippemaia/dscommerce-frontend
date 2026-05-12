ae59871a6c600ff2f52393b854d26eb6d1723eea
Philippe Maia <philippe.m.maia@hotmail.com>
2026-05-12 00:01:07 -0300

    Detalhes do produto dinâmico com parâmetro de rota

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -1,20 +1,23 @@
 import "./styles.css";
 import type { ProductDTO } from "../../models/product";
+import { Link } from "react-router-dom";
 
 type Props = {
   product: ProductDTO;
-}
+};
 
-export default function CatalogCard({product} : Props) {
+export default function CatalogCard({ product }: Props) {
   return (
-    <div class="dsc-card">
-      <div class="dsc-catalog-card-top dsc-line-bottom">
-        <img src={product.imgUrl} alt={product.name} />
+    <Link to={`/product-details/${product.id}`}>
+      <div class="dsc-card">
+        <div class="dsc-catalog-card-top dsc-line-bottom">
+          <img src={product.imgUrl} alt={product.name} />
+        </div>
+        <div class="dsc-catalog-card-bottom">
+          <h3>R$ {product.price.toFixed(2)}</h3>
+          <h4>{product.name}</h4>
+        </div>
       </div>
-      <div class="dsc-catalog-card-bottom">
-        <h3>R$ {product.price.toFixed(2)}</h3>
-        <h4>{product.name}</h4>
-      </div>
-    </div>
+    </Link>
   );
 }
