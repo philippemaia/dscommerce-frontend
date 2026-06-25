c8494e4bbc602cab813d8258deb2fdf6bc9e2d67
Philippe Maia <philippe.m.maia@hotmail.com>
2026-06-25 01:01:55 -0300

    Context contador no carrinho

---
+++ src/components/CartIcon/index.tsx
@@ -0,0 +1,19 @@
+import './styles.css';
+import { useContext } from "react";
+import cartIcon from "../../assets/cart.svg";
+import { ContextCartCount } from '../../utils/context-cart';
+
+export default function CartIcon() {
+  
+    const {contextCartCount} = useContext(ContextCartCount);
+
+    return (
+        <>
+            <img src={cartIcon} alt="Carrinho de compras" />
+            {
+                contextCartCount > 0 &&
+                <div className="dsc-cart-count">{contextCartCount}</div>
+            }
+        </>
+    );
+}
