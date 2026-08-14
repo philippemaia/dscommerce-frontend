64359cc775a54cf846d8a86a55e5ac47141b617d
Philippe Maia <philippe.m.maia@hotmail.com>
2026-08-14 00:41:32 -0300

    Função hasAnyRoles

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,54 +1,77 @@
 import QueryString from "qs";
-import type { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
+import type {
+  AccessTokenPayloadDTO,
+  CredentialsDTO,
+  RoleEnum,
+} from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import type { AxiosRequestConfig } from "axios";
-import { requestBackend } from '../utils/requests';
-import * as accessTokenRepository from '../localstorage/acess-token-repository';
+import { requestBackend } from "../utils/requests";
+import * as accessTokenRepository from "../localstorage/acess-token-repository";
 import jwtDecode from "jwt-decode";
 
-export function loginRequest(loginData: CredentialsDTO){
+export function loginRequest(loginData: CredentialsDTO) {
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+  };
 
-    const headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: "Basic " + window.btoa(CLIENT_ID + ":" + CLIENT_SECRET)
-    }
+  const requestBody = QueryString.stringify({
+    ...loginData,
+    grant_type: "password",
+  });
 
-    const requestBody = QueryString.stringify({...loginData, grant_type: "password"});
+  const config: AxiosRequestConfig = {
+    method: "POST",
+    url: "/oauth2/token",
+    data: requestBody,
+    headers,
+  };
 
-    const config: AxiosRequestConfig = {
-        method: "POST",
-        url: "/oauth2/token",
-        data: requestBody,
-        headers
-    }
-
-    return requestBackend(config);
+  return requestBackend(config);
 }
 
-export function logout(){
-    accessTokenRepository.remove();
+export function logout() {
+  accessTokenRepository.remove();
 }
 
-export function saveAccessToken(token: string){
-    accessTokenRepository.save(token);
+export function saveAccessToken(token: string) {
+  accessTokenRepository.save(token);
 }
 
-export function getAccessToken(){
-    return accessTokenRepository.get();
+export function getAccessToken() {
+  return accessTokenRepository.get();
 }
 
 export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
-    try {
-        const token = accessTokenRepository.get();
-        return token == null 
-            ? undefined 
-            : (jwtDecode(token) as AccessTokenPayloadDTO);
-    } catch (error) {
-        return undefined;
-    }
+  try {
+    const token = accessTokenRepository.get();
+    return token == null
+      ? undefined
+      : (jwtDecode(token) as AccessTokenPayloadDTO);
+  } catch (error) {
+    return undefined;
+  }
 }
 
 export function isAuthenticated(): boolean {
-    let tokenPayload = getAccessTokenPayload();
-    return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
+  let tokenPayload = getAccessTokenPayload();
+  return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
+}
+
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+  if (roles.length === 0) {
+    return true;
+  }
+
+  const tokenPayload = getAccessTokenPayload();
+  if (tokenPayload !== undefined) {
+    for (let i = 0; i < roles.length; i++) {
+      if (tokenPayload.authorities.includes(roles[i])) {
+        return true;
+      }
+    }
+    //return roles.some(role => tokenData.authorities.includes(role));
+  }
+  return false;
 }
